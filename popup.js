86e8b62d6a11715deb1f1c68483450c0578de211
nerdygoldy <akshayj3907@gmail.com>
2015-07-06 16:52:34 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 
 function myFun(){
-	//renderStatus("3");
+	
 	var port=chrome.runtime.connect({name: "my-channel"});
 
 	chrome.tabs.query({active:!0,currentWindow:!0},function(tbs){
@@ -12,19 +12,21 @@ function myFun(){
 					var searchUrl = 'http://www.omdbapi.com/?t='+response.msg+'&y=&plot=short&r=json';
 	  				var x = new XMLHttpRequest();
 	  				x.open('GET', searchUrl);
+	  				document.getElementById('idle').textContent = 'Searching...';
 	  				x.responseType = 'json';
 	  				x.onload = function() {
 	    
 	    				var movInfo = x.response;
-	    				console.log("hi");
-	    				console.log(movInfo.Error);
+	    				//console.log("hi");
+	    				//console.log(movInfo.Error);
+	    				document.getElementById('idle').textContent = '';
 	    				if(!movInfo.Error){
 	    					chrome.browserAction.setBadgeText({text: movInfo.imdbRating,tabId: tbs[0].id});
 	    					renderInfo(movInfo);		
 	    				}
 	    				else{
 	    					chrome.browserAction.setBadgeText({text: '',tabId: tbs[0].id});
-	    					document.getElementById('movTitle').textContent = "N/A";
+	    					//document.getElementById('movTitle').textContent = "N/A";
 	    				}
 	    				
 					};
@@ -42,12 +44,15 @@ function myFun(){
 
 
 function renderInfo(info) {
+	
 	document.getElementById('movTitle').textContent = info.Title;
 	document.getElementById('movGenre').textContent = info.Genre;
 	document.getElementById('movRuntime').textContent = info.Runtime;
+	document.getElementById('movLink').textContent = 'Click Here to see IMDB Page';
+	document.getElementById('movLink').href = 'www.imdb.com/'+info.imdbID;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
  	myFun();
- //renderStatus("2");
+ 
 });
